85ef79d73cb8777a76a98513d938ad898d514041
Linus Heckemann <git@sphalerite.org>
2019-10-04 16:39:28 +0200

    example: allow replacing xargs via env var

--- run-maper-example.sh
+++ run-maper-example.sh
@@ -52,4 +52,4 @@ launchlist-gen \
     -tgt-base $atlas -tgt-description $atlas/test-target.csv \
     -launchlist launchlist.sh || fatal "Launchlist not generated"
 
-time cat launchlist.sh | cut -d ' ' -f 2- | xargs -L 1 -P 1 maper | tee maper.log
+time cat launchlist.sh | cut -d ' ' -f 2- | ${xargs:-xargs -L 1 -P 1} maper | tee maper.log
